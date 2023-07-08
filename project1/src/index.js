207abf4a49bc11c3652b18486776cf9a94dc0aa5
raxitrangadiya <raxitrangadiya8531@gmail.com>
2023-07-08 23:30:21 +0530

    create a new project for router

--- project1/src/index.js
+++ project1/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './Header/Navbar/navbar.jsx';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Data from './data.jsx'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <>
-  <div>
-    <Navbar />
-  </div>
-    
+    < Data/>
   </>
 );
-

148d62550b6138a9143e8ef52309de4c60a5aae3
raxitrangadiya <raxitrangadiya8531@gmail.com>
2023-07-08 23:37:16 +0530

    data

--- project1/src/index.js
+++ project1/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import Data from './data.jsx'
+import Data from './navbar/navbar.jsx'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
--- project1/src/navbar/navbar.jsx
+++ project1/src/navbar/navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const data = () => {
     return (
         <div>
-            data
+            <h1>hellow world </h1>
         </div>
     );
 };

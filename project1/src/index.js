b174b8dc71d9a19bb0b84ceaf5551cf6f09c395b
raxitrangadiya <raxitrangadiya8531@gmail.com>
2023-07-09 23:48:59 +0530

    new

--- project1/src/index.js
+++ project1/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import Data from './navbar/navbar.jsx'
+import Navbar from './navbar/navbar.jsx'
+import Carousel from './Carousel/Carousel.jsx'
+import {  RouterProvider, } from "react-router-dom";
+import routerdata from './router.jsx'
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <>
-    < Data/>
-  </>
-);
+root.render(<>
+    < Navbar/> 
+    < Carousel/>
+    <RouterProvider router ={routerdata} />
+  </>);

1c46f9525367d699fc80c8393270db346c1c701e
raxitrangadiya <raxitrangadiya8531@gmail.com>
2023-08-17 10:27:05 +0530

    data

--- practice/src/component/Navbar/practice/classwork/FunctionCompoMenu/Intro.jsx
+++ practice/src/component/Navbar/practice/classwork/FunctionCompoMenu/Intro.jsx
@@ -6,20 +6,12 @@ class Intro extends Component {
             <>
                 <h2 className='text-center text-danger'>Function intro</h2>
                 <p>This function is a valid React component because it accepts a single <q>props</q> (which stands for properties) object argument with data and returns a React element. We call such components <q>function components</q>  because they are literally JavaScript functions.</p>
-<<<<<<< HEAD
 
-=======
->>>>>>> 0f215ed3cc1a9bce9d94d1bff53a913b1bbdfe33
                 <blockquote>
                     <span className='text-primary'>function</span> <span className='text-info'>Welcome(props)</span>  &#123; <br />
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; return &lt;h1&gt;Hello, &#123;props.name&#125;&lt;/h1&gt;; <br />
                     &#125;<br />
                 </blockquote>
-<<<<<<< HEAD
-
-                
-=======
->>>>>>> 0f215ed3cc1a9bce9d94d1bff53a913b1bbdfe33
             </>
         );
     }
